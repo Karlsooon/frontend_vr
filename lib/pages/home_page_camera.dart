1e11f936bdfce31ac7ecc2e77d75333b698e90c9
Karakat Almasova <karakatalmasova@Karakats-MacBook-Air.local>
2023-07-05 22:07:48 +0600

    added google lense api

--- lib/pages/home_page_camera.dart
+++ lib/pages/home_page_camera.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 import 'caption_page.dart';
+import 'dart:convert';
+import 'dart:developer';
 
 void main() {
   runApp(const MyApp());
@@ -109,43 +111,84 @@ class _GalleryAccessState extends State<GalleryAccess> {
     }
   }
 
+  Future<void> annotateImage(File imagePath, String apiKey) async {
+    final url = Uri.parse(
+        'https://vision.googleapis.com/v1/images:annotate?key=$apiKey');
+
+    final imageBytes = await imagePath.readAsBytes();
+    final base64Image = base64Encode(imageBytes);
+
+    final requestBody = jsonEncode({
+      'requests': [
+        {
+          'image': {'content': base64Image},
+          'features': [
+            {'type': 'LABEL_DETECTION'},
+            {'type': 'TEXT_DETECTION'},
+            {'type': 'FACE_DETECTION'},
+            {'type': 'WEB_DETECTION'},
+            {'type': 'PRODUCT_SEARCH'},
+
+            // Add more feature types as needed
+          ],
+        },
+      ],
+    });
+
+    final response = await http.post(url, body: requestBody);
+
+    if (response.statusCode == 200) {
+      // Successful response
+      final jsonResponse = jsonDecode(response.body);
+      print(response.body.toString());
+      print("*****************************************");
+      debugPrint(response.body.toString());
+      print("*****************************************");
+    } else {
+      // Handle other response codes
+      print('Error: ${response.body}');
+    }
+  }
+
   Future<void> sendImageToBackend(File imageFile) async {
+    annotateImage(imageFile, 'AIzaSyC9P-soLqC2RKaixM8JlP41A4LNf0YwxaQ');
+    return;
     var request = http.MultipartRequest(
       'POST',
-      Uri.parse('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyC9P-soLqC2RKaixM8JlP41A4LNf0YwxaQ'),
+      Uri.parse(
+          'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyC9P-soLqC2RKaixM8JlP41A4LNf0YwxaQ'),
     );
-          request.files.add(
-        await http.MultipartFile.fromPath('file', imageFile.path),
-      );
+    request.files.add(
+      await http.MultipartFile.fromPath('file', imageFile.path),
+    );
 
-      try {
-        var response = await request.send();
-        if (response.statusCode == 200) {
-          // Successful response
-          var responseJson = await response.stream.bytesToString();
-          // Parse the JSON response
-          var caption = parseCaption(responseJson);
-          // Display or process the caption as needed
-          print('Caption: $caption');
-        } else {
-          // Handle other response codes
-          print('Error: ${response.statusCode}');
-        }
-      } catch (e) {
-        print('Error: $e');
+    try {
+      var response = await request.send();
+      if (response.statusCode == 200) {
+        // Successful response
+        var responseJson = await response.stream.bytesToString();
+        // Parse the JSON response
+        var caption = parseCaption(responseJson);
+        // Display or process the caption as needed
+        print('Caption: $caption');
+      } else {
+        // Handle other response codes
+        print('Error: ${response.statusCode}');
       }
+    } catch (e) {
+      print('Error: $e');
     }
+  }
 
-    String parseCaption(String responseJson) {
-      // Parse the JSON response and extract the caption
-      // Modify this method based on the structure of your response JSON
-      // For example, if the response JSON has a key "caption" containing the caption string, you can use:
-      // var jsonResponse = jsonDecode(responseJson);
-      // var caption = jsonResponse['caption'];
-      // return caption;
+  String parseCaption(String responseJson) {
+    // Parse the JSON response and extract the caption
+    // Modify this method based on the structure of your response JSON
+    // For example, if the response JSON has a key "caption" containing the caption string, you can use:
+    // var jsonResponse = jsonDecode(responseJson);
+    // var caption = jsonResponse['caption'];
+    // return caption;
 
-      // For testing purposes, return a dummy caption
-      return 'Dummy Caption';
-    }
+    // For testing purposes, return a dummy caption
+    return 'Dummy Caption';
+  }
 }
-
