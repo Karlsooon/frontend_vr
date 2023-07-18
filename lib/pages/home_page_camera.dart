a11bee5527174257221fe58be631a28f38900dd9
Karakat Almasova <karakatalmasova@Karakats-MacBook-Air.local>
2023-07-18 15:36:32 +0600

    design of intro screen

--- lib/pages/home_page_camera.dart
+++ lib/pages/home_page_camera.dart
@@ -11,6 +11,7 @@ import 'package:flutter/services.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:vector_math/vector_math_64.dart' as vector_math;
+import 'package:camera/camera.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,34 +24,70 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.green),
-      home: const GalleryAccess(),
+      home: const CameraPage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class GalleryAccess extends StatefulWidget {
-  const GalleryAccess({Key? key});
+class CameraPage extends StatefulWidget {
+  const CameraPage({Key? key});
 
   @override
-  State<GalleryAccess> createState() => _GalleryAccessState();
+  _CameraPageState createState() => _CameraPageState();
 }
 
-class _GalleryAccessState extends State<GalleryAccess> {
+class _CameraPageState extends State<CameraPage> {
+  late CameraController _cameraController;
+  late Future<void> _initializeCameraController;
   File? galleryFile;
   final picker = ImagePicker();
   String backendResult = '';
 
   @override
+  void initState() {
+    super.initState();
+    _initializeCameraController = initCameraController();
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  Future<void> initCameraController() async {
+    final cameras = await availableCameras();
+    _cameraController = CameraController(cameras.first, ResolutionPreset.medium);
+    await _cameraController.initialize();
+    if (!mounted) return;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Gallery and Camera Access'),
+        title: const Text('Camera Access'),
         backgroundColor: const Color.fromARGB(255, 47, 66, 210),
-        actions: const [],
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Open settings page
+            },
+            icon: const Icon(Icons.settings),
+          ),
+        ],
       ),
       body: Stack(
         children: [
+          if (_cameraController.value.isInitialized)
+            Positioned.fill(
+              child: AspectRatio(
+                aspectRatio: _cameraController.value.aspectRatio,
+                child: CameraPreview(_cameraController),
+              ),
+            ),
           Positioned(
             top: 580,
             right: 20,
@@ -74,13 +111,7 @@ class _GalleryAccessState extends State<GalleryAccess> {
             left: 30,
             child: GestureDetector(
               onTap: () {
-                // _showPicker(context, ImageSource.camera);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ARKitExample(func: print),
-                  ),
-                );
+                _takePhoto();
               },
               child: Container(
                 width: 50,
@@ -93,31 +124,6 @@ class _GalleryAccessState extends State<GalleryAccess> {
               ),
             ),
           ),
-          Center(
-            child: SizedBox(
-              height: 200.0,
-              width: 300.0,
-              child: galleryFile == null
-                  ? const Center(child: Text('Sorry, nothing selected!'))
-                  : Image.file(galleryFile!),
-            ),
-          ),
-          if (galleryFile != null)
-            Positioned(
-              bottom: 0,
-              child: FractionalTranslation(
-                translation: const Offset(0, 0),
-                child: SizedBox(
-                  height: 300,
-                  width: 300,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(),
-                  ),
-                ),
-              ),
-            ),
-          // if (galleryFile != null)
-          //   ARKitExample(), // Add ARKitExample widget to display AR content
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -137,7 +143,17 @@ class _GalleryAccessState extends State<GalleryAccess> {
       });
     }
   }
-    Future<void> _sendImageToBackend() async {
+
+  void _takePhoto() async {
+    if (_cameraController.value.isInitialized) {
+      final image = await _cameraController.takePicture();
+      setState(() {
+        galleryFile = File(image.path);
+      });
+    }
+  }
+
+  Future<void> _sendImageToBackend() async {
     if (galleryFile != null) {
       var result = await getGptResultFromBackend(galleryFile!);
       setState(() {
@@ -149,7 +165,7 @@ class _GalleryAccessState extends State<GalleryAccess> {
   Future<String> getGptResultFromBackend(File imageFile) async {
     var request = http.MultipartRequest(
       'POST',
-      Uri.parse('http://localhost:8000/process_image'),
+      Uri.parse('http://35.234.108.24:8000/process_image'),
     );
     request.files.add(
       await http.MultipartFile.fromPath('file', imageFile.path),
@@ -226,110 +242,5 @@ class _ResultPageState extends State<ResultPage> {
         ),
       ),
     );
-  }
-}
-
-class ARKitExample extends StatefulWidget {
-  final Function func;
-  const ARKitExample({Key? key, required this.func}) : super(key: key);
-
-  @override
-  _ARKitExampleState createState() => _ARKitExampleState();
-}
-
-class _ARKitExampleState extends State<ARKitExample> {
-  late ARKitController arkitController;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    arkitController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          ARKitSceneView(
-            onARKitViewCreated: onARKitViewCreated,
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final image = await arkitController.snapshot();
-
-          var result = await getGptResultFromBackend(image);
-          print(result);
-          if (result == null) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ResultPage(result: result),
-              ),
-            );
-          }
-        },
-        child: const Icon(Icons.send),
-      ),
-    );
-  }
-
-  Future<int> getStreamLength(MemoryImage stream) async {
-    return stream.bytes.length;
-  }
-
-  Future<String> getGptResultFromBackend(ImageProvider<Object> image) async {
-    var request = http.MultipartRequest(
-      'POST',
-      Uri.parse('http://localhost:8000/process_image'),
-    );
-    final completer = Completer<Uint8List>();
-    final imageStream = image.resolve(ImageConfiguration.empty);
-
-    imageStream
-        .addListener(ImageStreamListener((imageInfo, synchronousCall) async {
-      final byteData =
-          await imageInfo.image.toByteData(format: ImageByteFormat.png);
-      final bytes = byteData!.buffer.asUint8List();
-      completer.complete(bytes);
-    }));
-
-    final bytes = await completer.future;
-    final multipartFile =
-        http.MultipartFile.fromBytes('image', bytes, filename: 'image.png');
-    request.files.add(multipartFile);
-    print(await multipartFile.finalize().length);
-    try {
-      var response = await request.send();
-      if (response.statusCode == 200) {
-        var caption = await response.stream.bytesToString();
-        return caption;
-      } else {
-        return 'Error: ${response.statusCode}';
-      }
-    } catch (e) {
-      return 'Error: $e';
-    }
-  }
-
-  void onARKitViewCreated(ARKitController arkitController) {
-    this.arkitController = arkitController;
-    addARKitNode(arkitController);
-  }
-
-  void addARKitNode(ARKitController arkitController) {
-    final node = ARKitNode(
-      geometry: ARKitSphere(radius: 0.1),
-      position: vector_math.Vector3(0, 0, -0.5),
-    );
-
-    arkitController.add(node);
   }
 }
--- lib/pages/intro_screen.dart
+++ lib/pages/intro_screen.dart
@@ -1,104 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'home_page_camera.dart';
+
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]); // Hide the status bar
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: [0.1, 0.6],
-            colors: [
-              Color(0xFF8058FA).withOpacity(0.88),
-              Color.fromARGB(255, 90, 48, 214),
-            ],
+          image: DecorationImage(
+            image: AssetImage('lib/images/your_background_image.png'),
+            fit: BoxFit.cover,
           ),
         ),
         child: SafeArea(
-          child: Column(
+          child: Stack(
             children: [
-              // big logo
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 100.0,
-                  right: 100.0,
-                  top: 120,
-                  bottom: 20,
-                ),
-                child: Image.asset('lib/images/vr_logo.png'),
-              ),
-
-              // we deliver groceries at your doorstep
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 100.0,
-                  right: 100.0,
-                  top: 1,
-                  bottom: 10,
-                ),
-                child: Text(
-                  'VR Lense',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.poppins(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+              Column(
+                children: [
+                  // big logo
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 100.0,
+                      right: 100.0,
+                      top: 100,
+                      bottom: 20,
+                    ),
+                    child: SizedBox(
+                      width: 354,
+                      height: 282,
+                      child: Image.asset('lib/images/vr_logo.png'),
+                    ),
                   ),
-                ),
-              ),
 
-              // groceree gives you fresh vegetables and fruits
-              Text(
-                'Get to know the world',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: const Color.fromARGB(255, 206, 189, 189),
-                ),
-              ),
-
-              const SizedBox(height: 24),
-
-              const Spacer(),
-
-              // get started button
-              GestureDetector(
-                onTap: () => Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return GalleryAccess();
-                    },
+                  // we deliver groceries at your doorstep
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 100.0,
+                      right: 100.0,
+                      top: 1,
+                      bottom: 1,
+                    ),
+                    child: Text(
+                      'AR Lense',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.only(
-                  left: 50.0,
-                  right: 50.0,
-                  top: 15,
-                  bottom: 15,
-                ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: const Color.fromARGB(255, 255, 255, 255),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 100.0,
+                      right: 100.0,
+                      top: 1,
+                      bottom: 50,
+                    ),
+                    child: Text(
+                      'Get to know the world',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: const Color.fromARGB(255, 206, 189, 189),
+                      ),
+                    ),
                   ),
-                  child: const Text(
-                    "Get Started",
-                    style: TextStyle(
-                      color: Color(0xFF8058FA),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+
+                  const SizedBox(height: 24),
+
+                  const Spacer(),
+                ],
+              ),
+              Positioned(
+                left: 76,
+                top: 630,
+                child: GestureDetector(
+                  onTap: () => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return CameraPage();
+                      },
+                    ),
+                  ),
+                  child: Container(
+                    width: 240,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xFFD9D9D9),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(
+                          color: Color(0xFF462B9C),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-
               const Spacer(),
             ],
           ),
